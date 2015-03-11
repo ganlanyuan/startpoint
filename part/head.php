f8c663db4ba4952fe2f8622369bd51240ebcd2a7
designer <ganlanyuan@gmail.com>
2015-03-11 13:47:12 -0400

    update .gitignore

--- part/head.php
+++ part/head.php
@@ -9,9 +9,6 @@
 <!--[if (IE 9)&!(IEMobile)]><html class="no-js lt-ie10" lang="en"><![endif]-->
 <!--[if gt IE 9]><!--> <html class="no-js" lang="en"><!--<![endif]-->
 <head>
-	<!-- Change this to match your local server hostname and path -->
-	<!-- <base href="http://localhost:8888/new/"> --><!--[if lte IE 6]></base><![endif]-->
-
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 	<!-- <meta http-equiv="cleartype" content="on"> -->
@@ -26,14 +23,6 @@
 	<!-- favicons -->
 	<link rel="shortcut icon" href="img/favicon.ico">
 	<link rel="shortcut icon" href="img/favicon.png">
-	<link rel="apple-touch-icon" href="img/touch_icon_60.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/touch-icon_72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="img/touch_icon_76.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/touch-icon_114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="img/touch_icon_120.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="img/touch_icon_152.png">
-	<link rel="apple-touch-startup-image" href="img/splash.png">
-	<!-- 152x152 img/touch_icon_152.png, 120x120 img/touch_icon_120.png, 114x114 img/touch-icon_114.png, 76x76 img/touch_icon_76.png, 72x72 img/touch-icon_72.png, 60x60 img/touch_icon_60.png, 32x32 img/favicon.png -->
 
 	<!-- Prefetch DNS for external assets -->
 	<link href="https://fontastic.s3.amazonaws.com" rel="dns-prefetch">
