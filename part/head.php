a87640c60dee1c7a6abde53c0ec0348407562ea4
William <ganlanyuan@gmail.com>
2016-06-29 22:53:27 -0400

    remove maximum-scale, user-scalable

--- part/head.php
+++ part/head.php
@@ -8,7 +8,7 @@
 <!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Site Title</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
